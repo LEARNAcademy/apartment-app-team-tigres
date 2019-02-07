21d6a02c87a1317bd1ef4758531b867e5b715561
Sam Sale <sam.sale@sky.com>
2019-02-07 12:00:27 -0800

    Added react and rendered first component

---
+++ appartment-app/app/javascript/components/MainApp.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PropTypes from "prop-types"
+class MainApp extends React.Component {
+  render () {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default MainApp
